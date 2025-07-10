2182bc15a0034cafb8914ad8f4579c71c59049fc
Harish Adepu <harishadepu40@gmail.com>
2025-07-10 13:34:19 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 import { AuthProvider } from './context/AuthContext.jsx'
-import { ChatProvider } from './context/chatContext.jsx'
+import { ChatProvider } from './context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
